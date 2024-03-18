df21384e9894b425ff4efa1e30407bc4591bf2f3
karthik Raj <61104188+karthikdedsec@users.noreply.github.com>
2024-03-18 11:59:57 +0530

    complete the popular categories section

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../../components/Banner";
+import Categories from "./Categories";
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <Categories />
     </div>
   );
 };
